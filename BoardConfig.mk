b17ab56dcba94e2727145068e5d3f00ed1340fbb
judyann63 <judyannrubia63@gmail.com>
2018-06-16 15:05:33 +0530

    BoardConfig: Android Features::Charger

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,3 +79,10 @@ TARGET_NEEDS_PROP_INIT_HACK := true
 
 # SELinux
 BOARD_SEPOLICY_DIRS += device/samsung/t-shark/sepolicy
+
+# Charger
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_NO_CHARGER_LED := true
+BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
+CHARGING_ENABLED_PATH := /sys/class/power_supply/battery/batt_lp_charging
+BACKLIGHT_PATH := /sys/class/backlight/panel/brightness
