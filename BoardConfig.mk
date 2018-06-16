509fb5aa83c21e45b37b1819feb3ec7904232f5d
judyann63 <judyannrubia63@gmail.com>
2018-06-16 15:52:54 +0530

    BoardConfig: Hardware Features::samsung

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,7 +12,6 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := cortex-a7
 TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
-
 TARGET_BOOTLOADER_BOARD_NAME := t-shark
 
 #Kernel 
